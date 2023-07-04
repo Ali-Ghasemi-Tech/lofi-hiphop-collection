c2c10dcc23b4cb5e16d34ad2374858a8dcd7486b
tlb <tlb@ubuntu.myguest.virtualbox.org>
2023-07-04 15:23:23 +0330

    riquest on click

---
+++ logic.js
@@ -0,0 +1,77 @@
+document.getElementById("lofi-girl").addEventListener("click" , function(){
+    document.querySelector(".play-area").innerHTML =
+     `<section class="play-area" id= "lofi-girl">
+     <iframe width="560" height="315" src="https://www.youtube.com/embed/jfKfPfyJRdk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    </section>`
+});
+
+
+document.getElementById("lofi-boy").addEventListener("click" , function(){
+    document.querySelector(".play-area").innerHTML =
+     `<section class="play-area" id= "lofi-boy">
+     <iframe width="560" height="315" src="https://www.youtube.com/embed/4xDzrJKXOOY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    </section>`
+});
+
+document.getElementById("The-Jazz-Hop-Cafe").addEventListener("click" , function(){
+    document.querySelector(".play-area").innerHTML =
+     `<section class="play-area" id= "The-Jazz-Hop-Cafe">
+     <iframe width="560" height="315" src="https://www.youtube.com/embed/uUIMrGGM968" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    </section>`
+});
+
+document.getElementById("the-bootleg-boy").addEventListener("click" , function(){
+    document.querySelector(".play-area").innerHTML =
+     `<section class="play-area" id= "the-bootleg-boy">
+     <iframe width="560" height="315" src="https://www.youtube.com/embed/0ba7dl40tSQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    </section>`
+});
+
+document.getElementById("chillhop-music").addEventListener("click" , function(){
+    document.querySelector(".play-area").innerHTML =
+     `<section class="play-area" id= "chillhop-music">
+     <iframe width="560" height="315" src="https://www.youtube.com/embed/5yx6BWlEVcY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    </section>`
+});
+
+document.getElementById("steezyasfuck").addEventListener("click" , function(){
+    document.querySelector(".play-area").innerHTML =
+     `<section class="play-area" id= "steezyasfuck">
+     <iframe width="560" height="315" src="https://www.youtube.com/embed/lP26UCnoH9s" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    </section>`
+});
+
+document.getElementById("dream-shop").addEventListener("click" , function(){
+    document.querySelector(".play-area").innerHTML =
+     `<section class="play-area" id= "dream-shop">
+     <iframe width="560" height="315" src="https://www.youtube.com/embed/wkhLHTmS_GI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    </section>`
+});
+
+document.getElementById("lofi-vibes").addEventListener("click" , function(){
+    document.querySelector(".play-area").innerHTML =
+     `<section class="play-area" id= "lofi-vibes">
+     <iframe width="560" height="315" src="https://www.youtube.com/embed/xjlYgH1O47g" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    </section>`
+});
+
+document.getElementById("little-soul").addEventListener("click" , function(){
+    document.querySelector(".play-area").innerHTML =
+     `<section class="play-area" id= "little-soul">
+     <iframe width="560" height="315" src="https://www.youtube.com/embed/XstikgbeTPI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    </section>`
+});
+
+document.getElementById("chilli-music").addEventListener("click" , function(){
+    document.querySelector(".play-area").innerHTML =
+     `<section class="play-area" id= "chilli-music">
+     <iframe width="560" height="315" src="https://www.youtube.com/embed/gKqv41OXhAE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    </section>`
+});
+
+document.getElementById("tony").addEventListener("click" , function(){
+    document.querySelector(".play-area").innerHTML =
+     `<section class="play-area" id= "tony">
+     <iframe width="560" height="315" src="https://www.youtube.com/embed/jxPLkfyOZGg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    </section>`
+});
